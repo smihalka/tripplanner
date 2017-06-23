b80d4e66bb244645c8c46fc629c073020adb08db
Kevin Genus <kevin.genus@gmail.com>
2017-06-22 23:12:14 -0400

    added db pooling

--- models/index.js
+++ models/index.js
@@ -1,4 +1,10 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost/tripplanner');
+const db = new Sequelize('postgres://localhost/tripplanner', {
+    pool: {
+        maxConnections: 5,
+        minConnections: 0,
+        maxIdleTime: 30
+    }
+});
 
 module.exports = db;
